6a72fb4399fc320e409f8e62c1fecfbef5f7accb
Richard Smith <ricardo.smythe@gmail.com>
2016-02-08 12:18:22 +0000

    Initial Commit

---
+++ dbtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dbtest
+//
+//  Created by Richard Smith on 08/02/2016.
+//  Copyright © 2016 Kinch Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
